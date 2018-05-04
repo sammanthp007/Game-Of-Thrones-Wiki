e45548097570805624b2da988715c93f9fa495b6
sammanthp007 <sammanbikramthapa@gmail.com>
2018-05-04 18:51:22 -0400

    Create AllCharactersViewController as an empty UIViewController

---
+++ Game Of Thrones Wiki/ViewControllers/AllCharactersViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AllCharactersViewController.swift
+//  Game Of Thrones Wiki
+//
+//  Created by Samman Thapa on 5/4/18.
+//  Copyright © 2018 Samman Labs. All rights reserved.
+//
+
+import UIKit
+
+class AllCharactersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
